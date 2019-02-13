4592a29b78cdfbbf4a8f1473c2025ac470b3f746
Denis Pukhovskiy <d.pukhovskiy@cti.ru>
2019-02-14 01:45:42 +0300

    local save

--- __tests__/compare-files.test.js
+++ __tests__/compare-files.test.js
@@ -2,9 +2,10 @@ import fs from 'fs';
 import compareFiles from '../src';
 
 const testsDirPath = '__tests__/__fixtures__';
-const fileTypes = ['.json', '.yml', '.ini'];
+//const fileTypes = ['.json', '.yml', '.ini'];
+const fileTypes = ['.json'];
 
 test.each(fileTypes)('compare %s files', (type) => {
-  const filesDiff = fs.readFileSync(`${testsDirPath}/result`, 'utf8');
-  expect(compareFiles(`${testsDirPath}/before${type}`, `${testsDirPath}/after${type}`)).toBe(filesDiff);
+  const filesDiff = fs.readFileSync(`${testsDirPath}/r`, 'utf8');
+  expect(compareFiles(`${testsDirPath}/b${type}`, `${testsDirPath}/a${type}`)).toBe(filesDiff);
 });
--- src/index.js
+++ src/index.js
@@ -3,28 +3,108 @@ import _ from 'lodash';
 import { extname } from 'path';
 import parse from './parsers';
 
-const compareObjects = (obj1, obj2) => {
-  const lineBuilders = {
-    buildEqual: key => `  ${key}: ${obj1[key]}`,
-    buildAdded: key => `+ ${key}: ${obj2[key]}`,
-    buildDeleted: key => `- ${key}: ${obj1[key]}`,
-  };
-  const keys = _.union(_.keys(obj1), _.keys(obj2));
-  const objectDiffs = keys.map((key) => {
+const buildAST = (object1, object2) => {
+  const keys = _.union(_.keys(object1), _.keys(object2));
+  const buildDiff = (key, obj1, obj2) => {
+    
     if (_.has(obj1, key) && _.has(obj2, key)) {
-      return obj2[key] === obj1[key]
-        ? lineBuilders.buildEqual(key)
-        : `${lineBuilders.buildAdded(key)}\n ${lineBuilders.buildDeleted(key)}`;
+      const value1 = obj1[key];
+      const value2 = obj2[key];
+
+      if (_.isObject(value1) && _.isObject(value2)) {
+        const children = _.flatten(buildAST(value1, value2));
+        return {
+          key,
+          children,
+          type: 'unchanged',
+          value: null,
+        };
+      }
+
+      if (value1 === value2) {
+        return {
+          key,
+          children: [],
+          type: 'unchanged',
+          value: value1,
+        };
+      }
+
+      return [
+        {
+          key,
+          children: [],
+          type: 'added',
+          value: value2,
+        },
+        {
+          key,
+          children: [],
+          type: 'deleted',
+          value: value1,
+        },
+      ];
     }
-    return _.has(obj2, key) ? lineBuilders.buildAdded(key) : lineBuilders.buildDeleted(key);
-  });
-  return `{\n ${objectDiffs.join('\n ')}\n}`;
+    if (_.has(obj2, key)) {
+      return {
+        key,
+        children: [],
+        type: 'added',
+        value: _.isObject(obj2[key]) ? obj2[key] : obj2[key].toString(),
+      };
+    }
+    return {
+      key,
+      children: [],
+      type: 'deleted',
+      value: _.isObject(obj1[key]) ? obj1[key] : obj1[key].toString(),
+    };
+
+    
+  };
+  return keys.map(key => (buildDiff(key, object1, object2)));
 };
+
+
+const prefix = {
+  unchanged: ' ',
+  added: '+',
+  deleted: '-',
+};
+
+const buildLine = (type, key, value) => `${prefix[type]} ${key}: ${value}`;
+
+const stringify = (obj) => {
+  const strings = Object.keys(obj).map(key => `${key}: ${obj[key]}`);
+  return `{\n ${strings.join('\n')}\n}`;
+};
+
+
+const renderAST = (data) => {
+  
+  
+  const diff = data.map((item) => {
+    
+    if (item.value && !_.isObject(item.value)) {
+      return buildLine(item.type, item.key, item.value);
+    }
+    if (_.isObject(item.value)) {
+      return buildLine(item.type, item.key, stringify(item.value));
+    }
+    return `${prefix[item.type]} ${item.key}: ${renderAST(item.children)}`;
+  });
+
+
+  return `{\n ${diff.join('\n ')} \n}`;
+}
 
 const compareFiles = (filePath1, filePath2) => {
   const obj1 = parse(fs.readFileSync(filePath1, 'utf8'), extname(filePath1));
   const obj2 = parse(fs.readFileSync(filePath2, 'utf8'), extname(filePath2));
-  return compareObjects(obj1, obj2);
+  //return compareObjects(obj1, obj2);
+  const ast = _.flatten(buildAST(obj1, obj2));
+  const diff = renderAST(ast);
+  return diff;
 };
 
 export default compareFiles;
